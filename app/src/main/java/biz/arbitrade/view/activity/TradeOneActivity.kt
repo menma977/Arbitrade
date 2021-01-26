3341d288d993056ca2cf7013b8a1ef4e8c318732
SusuTawar <realsusutawar@gmail.com>
2021-01-27 06:06:42 +0700

    basic trade ui

---
+++ app/src/main/java/biz/arbitrade/view/activity/TradeOneActivity.kt
@@ -0,0 +1,12 @@
+package biz.arbitrade.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import biz.arbitrade.R
+
+class TradeOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trade_one)
+    }
+}
---
+++ app/src/main/java/biz/arbitrade/view/activity/TradeTwoActivity.kt
@@ -0,0 +1,12 @@
+package biz.arbitrade.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import biz.arbitrade.R
+
+class TradeTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trade_two)
+    }
+}
