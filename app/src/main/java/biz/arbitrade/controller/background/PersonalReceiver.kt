ce8c2604fc7fc7f28ee41f4b2f930fe9a9e04d8e
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2021-02-06 16:39:11 +0700

    Update PersonalReceiver.kt

--- app/src/main/java/biz/arbitrade/controller/background/PersonalReceiver.kt
+++ app/src/main/java/biz/arbitrade/controller/background/PersonalReceiver.kt
@@ -11,10 +11,13 @@ import com.pusher.client.Pusher
 import com.pusher.client.PusherOptions
 import com.pusher.client.channel.PrivateChannelEventListener
 import com.pusher.client.channel.PusherEvent
+import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionState
+import com.pusher.client.connection.ConnectionStateChange
 import com.pusher.client.util.HttpAuthorizer
 import java.lang.Exception
 
-class PersonalReceiver() : Service() {
+class PersonalReceiver : Service() {
   override fun onBind(intent: Intent): IBinder {
     TODO("Return the communication channel to the service.")
   }
@@ -25,37 +28,45 @@ class PersonalReceiver() : Service() {
     val user = User(this)
     val header = HashMap<String, String>()
     header["Authorization"] = "Bearer ${user.getString("token")}"
+    Log.d("pusher", "TOKEN : ${user.getString("token")}")
     val authorize = HttpAuthorizer(Url.Pusher.auth())
     authorize.setHeaders(header)
-    val options =
-      PusherOptions().setHost(Url.Pusher.url).setWsPort(Url.Pusher.port).setAuthorizer(authorize)
-        .setUseTLS(Url.Pusher.secured) //.setCluster("mt1")
-    val pusher = Pusher("arib.biz.key", options)
-    Log.d("pusher", options.buildUrl(Url.Pusher.auth()))
-    Log.d("pusher", "-pusher.subscribePrivate \"private-arbi.biz.${user.getString("username")}\"")
-    val personalChannel =
-      pusher.subscribePrivate("private-arbi.biz.admin", object :
-        PrivateChannelEventListener {
-        override fun onEvent(event: PusherEvent?) {
-          Log.d("pusher", "-onEvent")
-          //TODO("Not yet implemented")
+    val options = PusherOptions().setHost(Url.Pusher.url).setWsPort(Url.Pusher.port).setAuthorizer(authorize).setUseTLS(Url.Pusher.secured) //.setCluster("mt1")
+    val privatePusher = Pusher("arib.biz.key", options)
+
+    privatePusher.connect(object : ConnectionEventListener {
+      override fun onConnectionStateChange(change: ConnectionStateChange) {
+        println(
+          "pv State changed to " + change.currentState + " from " + change.previousState
+        )
+        if (change.currentState == ConnectionState.CONNECTED) {
+          val personalChannel = privatePusher.subscribePrivate("private-arbi.biz.admin", object : PrivateChannelEventListener {
+            override fun onEvent(event: PusherEvent) {
+              println("pusher")
+              Log.d("pusher", event.data)
+            }
+
+            override fun onSubscriptionSucceeded(channelName: String?) {
+              println("pusher2")
+              Log.d("pusher", "private sub $channelName success")
+            }
+
+            override fun onAuthenticationFailure(message: String?, e: Exception?) {
+              println("pusher3")
+              Log.e("pusher", "private sub $message fail $e")
+              user.setString("username", message ?: "Error")
+            }
+          })
+
+          OnTicket(application, personalChannel).bind()
         }
+      }
 
-        override fun onSubscriptionSucceeded(channelName: String?) {
-          Log.d("pusher", "-onSubscriptionSucceeded")
-          //TODO("Not yet implemented")
-        }
-
-        override fun onAuthenticationFailure(message: String?, e: Exception?) {
-          Log.d("pusher", "-onAuthenticationFailure")
-          //TODO("Not yet implemented")
-        }
-
-      })
-
-    pusher.connect()
-
-    OnTicket(this, personalChannel).bind()
+      override fun onError(message: String?, code: String?, e: Exception?) {
+        println("Personal Receiver There was a problem connecting!")
+        Log.e("pusher", "private sub $message fail $e code $code")
+      }
+    })
   }
 
 }
