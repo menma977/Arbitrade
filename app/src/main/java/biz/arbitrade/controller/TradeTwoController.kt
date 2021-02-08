4dac59e7ca3cdcdfad9f5f2de7ae35ab3eb82355
SusuTawar <realsusutawar@gmail.com>
2021-02-09 04:43:34 +0700

    fixing inccorect profit

--- app/src/main/java/biz/arbitrade/controller/TradeTwoController.kt
+++ app/src/main/java/biz/arbitrade/controller/TradeTwoController.kt
@@ -2,16 +2,16 @@ package biz.arbitrade.controller
 
 import biz.arbitrade.network.ArbizAPI
 import biz.arbitrade.network.DogeAPI
-import kotlin.math.abs
-import kotlin.random.Random
 import okhttp3.FormBody
 import org.json.JSONObject
+import kotlin.math.abs
+import kotlin.random.Random
 
 /**
  * 999doge bet controller
  * @param winTarget target win from initial balance in percent
  */
-class TradeTwoController(private val winTarget: Float) {
+class TradeTwoController() {
   private val martingaleWinningIncrement = .1
   private var totalProfit: Long = 0
 
@@ -31,18 +31,29 @@ class TradeTwoController(private val winTarget: Float) {
     return DogeAPI(form).call()
   }
 
-  fun store(token: String, satoshi: Long, target: Long, low: Int, high: Int, data: JSONObject): JSONObject {
+  fun store(
+    token: String,
+    satoshi: Long,
+    target: Long,
+    low: Int,
+    high: Int,
+    finish: Boolean,
+    data: JSONObject
+  ): JSONObject {
     val body = FormBody.Builder()
     val res = data.getJSONObject("data") // TODO: change accordingly
     body.add("start_balance", res.getString("StartingBalance"))
     body.add("end_balance", res.getString("Balance"))
     body.add("target_balance", target.toString())
     body.add("pay_in", satoshi.toString())
-    body.add("pay_out", "${res.getLong("Payout") - res.getLong("StartingBalance")}")
+    body.add("pay_out", "${res.getLong("Payout") - satoshi}")
     body.add("low", low.toString())
     body.add("high", high.toString())
-    body.add("status", "")
-    body.add("is_finish", false.toString())
+    body.add(
+      "status",
+      if (satoshi < (res.getLong("Payout") - satoshi)) "lose" else "win"
+    )
+    body.add("is_finish", finish.toString())
     return ArbizAPI("martiangle.store", "POST", token, body).call()
   }
 
