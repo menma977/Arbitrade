997da42be3daf695d563ef5528ca3b3f85abfc62
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2021-02-09 14:51:44 +0700

    Update SettingFragment.kt

--- app/src/main/java/biz/arbitrade/view/fragments/SettingFragment.kt
+++ app/src/main/java/biz/arbitrade/view/fragments/SettingFragment.kt
@@ -20,7 +20,7 @@ class SettingFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
-    parentActivity = HomeActivity()
+    parentActivity = activity as HomeActivity
 
     user = User(parentActivity.applicationContext)
 
