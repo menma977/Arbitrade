4d2f7365d178daf528d5d460b4236316b8a0f033
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2021-02-11 15:44:40 +0700

    Update OnTicket.kt

--- app/src/main/java/biz/arbitrade/controller/events/OnTicket.kt
+++ app/src/main/java/biz/arbitrade/controller/events/OnTicket.kt
@@ -1,13 +1,9 @@
 package biz.arbitrade.controller.events
 
-import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import biz.arbitrade.MainActivity
-import biz.arbitrade.controller.Helper
-import biz.arbitrade.controller.Notification
 import biz.arbitrade.model.User
 import com.pusher.client.channel.Channel
 import org.json.JSONObject
@@ -18,7 +14,7 @@ class OnTicket(context: Context, channel: Channel) : PusherEvent(context, channe
   override fun handle(context: Context, result: JSONObject) {
     val user = User(context)
     val intent = Intent(context, MainActivity::class.java)
-    user.setLong("totalPin", 0)
+    user.setLong("totalPin", result.getLong("ticket"))
     intent.action = "ticket"
     LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
   }
