ef5074fcdfdbd7f34909fb01610c069698a17b6d
SusuTawar <realsusutawar@gmail.com>
2021-02-11 17:06:05 +0700

    cleanup

--- app/src/main/java/biz/arbitrade/controller/events/OnBot.kt
+++ app/src/main/java/biz/arbitrade/controller/events/OnBot.kt
@@ -1,13 +1,6 @@
 package biz.arbitrade.controller.events
 
-import android.app.NotificationManager
 import android.content.Context
-import android.content.Intent
-import android.util.Log
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import biz.arbitrade.MainActivity
-import biz.arbitrade.controller.Helper
-import biz.arbitrade.controller.Notification
 import biz.arbitrade.model.User
 import com.pusher.client.channel.Channel
 import org.json.JSONObject
